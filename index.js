1e321fcea9c9ea46196511f7a33dd8bf511f2642
mohit.kumar <mohit.kumar@trangile.com>
2025-02-18 12:01:33 +0530

    minor fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-require("dotenv").config();
 const userRoutes = require("./routes/userRoutes");
 const cors = require("cors");
 
@@ -12,7 +12,10 @@ app.use(cors());
 
 // MongoDB Connection
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Error connecting to MongoDB:", err));
 
